bd865f19dc9cbc18da99498cceca20b65eb6de2b
Winston Saunders <winstonasaunders@gmail.com>
2016-08-22 07:42:22 -0700

    Bot works with oauth

--- SantiamBot.R
+++ SantiamBot.R
@@ -6,3 +6,27 @@
 ##
 ##
 library(twitteR)
+library(dplyr)
+
+
+## create URLs
+reqURL <- "https://api.twitter.com/oauth/request_token"
+accessURL <- "https://api.twitter.com/oauth/access_token"
+authURL <- "https://api.twitter.com/oauth/authorize"
+
+## Keys
+## you get these from apps.twitter.com
+
+directory <- "/Users/winstonsaunders/Documents/SantiamBot/"
+sub_directory <- "secrets/"
+
+keys <- read.csv(paste0(directory, sub_directory, "secret.key.csv"), encoding = "UTF-8", stringsAsFactors = FALSE)
+
+
+consumerKey <- keys$consumerKey
+consumerSecret <- keys$consumerSecret
+accessToken <- keys$accessToken
+accessSecret <- keys$accessSecret
+
+## set up authentication
+setup_twitter_oauth(consumerKey, consumerSecret, accessToken, accessSecret)
