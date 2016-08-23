0402bdcae1c489f3d1d8f95a082e101aba7407e5
Winston Saunders <winstonasaunders@gmail.com>
2016-08-23 16:51:39 -0700

    getting cleaning to work right

--- SantiamBot.R
+++ SantiamBot.R
@@ -80,7 +80,7 @@ library(dplyr)
     ##define user
     user <- getUser(paste0("@", name))
     ## get the data
-    user_data<-userTimeline(user, n=1000, maxID=NULL, sinceID=NULL, includeRts=FALSE)
+    user_data<-userTimeline(user, n=2000, maxID=NULL, sinceID=NULL, includeRts=FALSE)
     ##inspect it
     head(user_data)
     ## convert to a data frame
@@ -89,11 +89,60 @@ library(dplyr)
     
 ## CLEAN AND TRANSFORM DATA    
     
+    
+    ## dedupe
+    hwy_df<-hwy_df[!duplicated(hwy_df$created),]
+    ##keep only columns of interest
+    hwy_df<-hwy_df[,c("created", "text")]
+    
+    ## GOOD TO HERE>>.
+    
     ## convert created column to date format
     hwy_df$created<-as.Date(hwy_df$created)
     
     ## dedupe
     hwy_df<-hwy_df[!duplicated(hwy_df$created),]
+    ##keep only columns of interest
+    hwy_df<-hwy_df[,c("created", "text")]
+    
+    
+    #hwy_df$created<-as.character(hwy_df$created)
+    hwy_df$text<-as.character(hwy_df$text)
+    
+    ##make the date column a date
+    #hwy_df$date<-as.Date(hwy_df$created)
+    ## create pacific time column
+    hwy_df$PDT<-as.POSIXct(hwy_df$created, tz="UTC")
+    ## convert to Pacific time
+    attributes(hwy_df$PDT)$tzone<-"US/Pacific"
+    
+    ##make an "hour" column (for time of day)
+    library(lubridate)
+    t.lub <- ymd_hms(hwy_df$PDT)
+    hwy_df$hour <- hour(t.lub) + minute(t.lub)/60
+    
+    ## make a seconds column
+    hwy_df$chronsecs<-as.numeric(as.POSIXlt(hwy_df$PDT))
+    
+    
+    ## create "dayperiod"
+    hwy_df$dayperiod<-NA
+    hwy_df$dayperiod[hwy_df$hour>=3 & hwy_df$hour<9]<-"morning"
+    hwy_df$dayperiod[hwy_df$hour>=9 & hwy_df$hour<15]<-"midday"
+    hwy_df$dayperiod[hwy_df$hour>=15 & hwy_df$hour<21]<-"evening"
+    hwy_df$dayperiod[hwy_df$hour>=19]<-"night"
+    hwy_df$dayperiod[hwy_df$hour<3]<-"night"
+    
+    ## make a factor
+    hwy_df$dayperiod<-as.factor(hwy_df$dayperiod)
+    #head(hwy_df)
+    
+    
+    
+    ### capture start and edn times of period 
+    tEnd <- as.Date(hwy_df$created)[length(hwy_df$created)]
+    tStart <- as.Date(hwy_df$created[1])
+    
     
     ## look for specific incidents and create tidy data frames
     
@@ -112,4 +161,6 @@ library(dplyr)
 ## GENERATE MODEL    
     
     
+        
+        
     
